3537b0553d64b2a3e0e758bf16c304b5a4b9632f
Sergio <sfernandez@leadclic.com>
2023-10-23 07:56:04 -0300

    add lwc visuals community

---
+++ force-app/main/default/lwc/communityConstants/communityConstants.js
@@ -0,0 +1,6 @@
+export const SUCCESS = 'success';
+export const ERROR = 'error';
+export const WARNING = 'warning';
+export const GD_DOCUMENT_ITEMS_NAMESPACE = 'gdDocumentItemsCommunity';
+export const GD_DOCUMENT_SUMMARY_NAMESPACE = 'gdDocumentSummary';
+export const LMS_OPEN_CHANNEL = 'c-lms-message-service';
---
+++ force-app/main/default/lwc/gdDocumentSummary/labels.js
@@ -0,0 +1,27 @@
+import GDIRPH_GDDocumentsToProvide from '@salesforce/label/c.GDIRPH_GDDocumentsToProvide';
+import GDIRPH_PendingDocuments from '@salesforce/label/c.GDIRPH_PendingDocuments';
+import GDIRPH_RequiredAditionalDocumentsTitle from '@salesforce/label/c.GDIRPH_RequiredAditionalDocumentsTitle';
+import GDIRPH_ConsumerSupportingDocuments from '@salesforce/label/c.GDIRPH_ConsumerSupportingDocuments';
+import GDIRPH_SpecialDivorceDocuments from '@salesforce/label/c.GDIRPH_SpecialDivorceDocuments';
+import GDIRPH_SpecialCaseInheritanceDocuments from '@salesforce/label/c.GDIRPH_SpecialCaseInheritanceDocuments';
+import GDIRPH_SpecialDisabilityCaseDocuments from '@salesforce/label/c.GDIRPH_SpecialDisabilityCaseDocuments';
+import GDIRPH_ConsumerSupportingDocumentsValues from '@salesforce/label/c.GDIRPH_ConsumerSupportingDocumentsValues';
+import GDIRPH_SpecialDivorceDocumentsValues from '@salesforce/label/c.GDIRPH_SpecialDivorceDocumentsValues';
+import GDIRPH_SpecialCaseInheritanceDocumentsValues from '@salesforce/label/c.GDIRPH_SpecialCaseInheritanceDocumentsValues';
+import GDIRPH_SpecialDisabilityCaseDocumentsValues from '@salesforce/label/c.GDIRPH_SpecialDisabilityCaseDocumentsValues';
+import GDIRPH_NoPendingDocuments from '@salesforce/label/c.GDIRPH_NoPendingDocuments';
+
+export const Label = {
+    documentsToProvide : GDIRPH_GDDocumentsToProvide,
+    noPendingDocuments : GDIRPH_NoPendingDocuments,
+    pendingDocumentTitle : GDIRPH_PendingDocuments,
+    requiredAditionalDocumentsTitle : GDIRPH_RequiredAditionalDocumentsTitle,
+    consumerSupportingDocuments : GDIRPH_ConsumerSupportingDocuments,
+    consumerSupportingDocumentsValues : GDIRPH_ConsumerSupportingDocumentsValues,
+    specialCaseInheritanceDocuments : GDIRPH_SpecialCaseInheritanceDocuments,
+    specialCaseInheritanceDocumentsValues : GDIRPH_SpecialCaseInheritanceDocumentsValues,
+    specialDivorceDocuments : GDIRPH_SpecialDivorceDocuments,
+    specialDivorceDocumentsValues : GDIRPH_SpecialDivorceDocumentsValues,
+    specialDisabilityCaseDocuments : GDIRPH_SpecialDisabilityCaseDocuments,
+    specialDisabilityCaseDocumentsValues : GDIRPH_SpecialDisabilityCaseDocumentsValues,
+}
