6b95771e88c36719588c2328c6ef987e6717b47a
Sergio <sfernandez@leadclic.com>
2023-10-23 13:01:56 -0300

    add grid wrapper fix

--- force-app/main/default/lwc/gdDocumentSummaryLine/gdDocumentSummaryLine.js
+++ force-app/main/default/lwc/gdDocumentSummaryLine/gdDocumentSummaryLine.js
@@ -7,4 +7,26 @@ export default class GdDocumentSummaryLine extends LightningElement {
     handleSelected(event) {
         this.dispatchEvent(new CustomEvent('selected', {detail : this.line}));
     }
+
+    /**
+     * Si tiene ayuda el documento, aseguramos el espacio
+     * para el help-text, de lo contrario ocupamos la columna entera
+     */
+    get titleClass() {
+        if (this.line.helpText) {
+            return 'slds-size_11-of-12 slds-truncate';
+        }
+        return 'slds-truncate';
+    }
+
+    /**
+     * Si está aportado el documento, aseguramos el espacio
+     * para el estado, de lo contrario ocupamos la columna entera
+     */
+    get titleWrapperClass() {
+        if (!this.line.visible) {
+            return 'slds-size_9-of-12 slds-text-align_left';
+        }
+        return 'slds-text-align_left';
+    }
 }
